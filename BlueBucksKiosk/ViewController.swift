3d4eeed4f71256a9b72b431c8077eebfbb1c9768
KJH <gh7052@gmail.com>
2024-04-01 17:51:40 +0900

    Initial commit: BlueBucksKiosk

---
+++ BlueBucksKiosk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlueBucksKiosk
+//
+//  Created by 김정호 on 4/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
