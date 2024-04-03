03af1a83e0cb9861625f6531bbaa47f7bfd10293
yoonhee <parkyunhee1217@gmail.com>
2024-04-03 15:29:13 +0900

    주석추가
    Fix: 예시 음료명, 가격 변경
    Add: 주석추가

--- BlueBucksKiosk/Detail/DetailPageViewController.swift
+++ BlueBucksKiosk/Detail/DetailPageViewController.swift
@@ -2,7 +2,7 @@
 //  DetailPageViewController.swift
 //  BlueBucksKiosk
 //
-//  Created by SAMSUNG on 2024/04/02.
+//  Created by 박윤희 on 2024/04/02.
 //
 
 import UIKit
@@ -17,19 +17,20 @@ class DetailPageViewController: UIViewController {
     @IBOutlet var descriptionText: UILabel!
     
     
-    
+    //베이직 struct
     struct Drink {
         var name: (String, String)
         var image: UIImage
         var description: String = ""
         var price: Int
     }
-    
+    //선택한 제품의 이미지, 이름, 가격
     var selectedImage: UIImage?
-    var selectedName: String = "제품명"
-    var selectedPrice: Int = 0
+    var selectedName: String = "아메리카노"
+    var selectedPrice: Int = 5000
     
-    let productDescription: [String: String] = [
+    //음료명과 상세설명 매핑 ["음료명": "상세섦명"]
+    let drinkDescription: [String: String] = [
         "아이스 아메리카노": "블루벅스만의 깔끔하고 강렬한 에스프레소를 가장 부드럽고 시원하게 즐길 수 있는 커피"
                                               ]
 
@@ -37,48 +38,36 @@ class DetailPageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        //이전 뷰컨트롤러에 있는 정보 가져오기
         imageView.image = selectedImage
         nameLabel.text = selectedName
         priceLabel.text = "\(selectedPrice)₩"
         
-        if let drinkDescription = productDescription[selectedName]{
+        //음료명에 따른 상세설명 표시
+        if let drinkDescription = drinkDescription[selectedName]{
             descriptionText.text = drinkDescription
         }else{
             descriptionText.text = "상세설명 없음"
         }
     }
    
-    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // if segue.identifier == "세그웨이 식별자 이름"
-//        if segue.identifier == ""{
-//            //if let nextVC = segue.destination as? 다음 뷰 컨트롤러 이름
-//            if let nextVC = segue.destination as? _ {
-//                nextVC.selectedName = selectedName
-//                nextVC.
-//            }
-//        }
-//    }
-//
-//
-//
-//
-//    @IBAction func tappedInto(_ sender: Any) {
-//        //performSegue(withIdentifier: "세그웨이 식별자 이름", sender: nil)
-//        performSegue(withIdentifier: <#T##String#>, sender: nil)
-//
-//    }
-//
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    //segue로 다음 뷰컨트롤러에 정보 전달
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // if segue.identifier == "세그웨이 식별자 이름"
+        if segue.identifier == ""{
+            //if let nextVC = segue.destination as? 다음 뷰 컨트롤러 이름
+            if let nextVC = segue.destination as? _ {
+                nextVC.selectedName = selectedName
+                nextVC.selectedPrice = selectedPrice
+            }
+        }
     }
-    */
+
+    //버튼을 누르면 다음 뷰컨트롤러로 이동 및 정보 전달
+    @IBAction func tappedInto(_ sender: UIButton) {
+        //performSegue(withIdentifier: "세그웨이 식별자 이름", sender: self)
+        performSegue(withIdentifier: <#T##String#>, sender: self)
+
+    }
 
 }
