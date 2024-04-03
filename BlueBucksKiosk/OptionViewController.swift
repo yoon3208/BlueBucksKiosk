31d2bb6e99bbe912f4368d3d3e2a20293353ed57
hyelim9300 <hye930011@naver.com>
2024-04-03 19:41:31 +0900

    Add : Option

---
+++ BlueBucksKiosk/OptionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OptionViewController.swift
+//  BlueBucksKiosk
+//
+//  Created by 서혜림 on 4/3/24.
+//
+
+import UIKit
+
+class OptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
