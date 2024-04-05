a313125a9f4a89d28d8ad4239f9632d66b2c64cb
yoonhee <parkyunhee1217@gmail.com>
2024-04-05 10:47:35 +0900

    Fix: 이전 뷰컨트롤러 및 더미데이터 정보를  받고 보내는 코드 수정, 스토리보드 Button크기 변경

--- BlueBucksKiosk/Controller/DetailPageViewController.swift
+++ BlueBucksKiosk/Controller/DetailPageViewController.swift
@@ -15,19 +15,18 @@ class DetailPageViewController: UIViewController {
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet var descriptionText: UILabel!
     
-    
-   
-    //선택한 제품의 이미지, 이름, 가격
-    var Image: UIImage?
-    var name: String = "아이스 아메리카노"
-    var price: Int = 4500
-    
-    //음료명과 상세설명 매핑 ["음료명": "상세섦명"]
-    let descriptions: [String: String] = [
-        "아이스 아메리카노": "블루벅스만의 깔끔하고 강렬한 에스프레소를 가장 부드럽고 시원하게 즐길 수 있는 커피"
-                                              ]
+    //뷰 컨트롤러의 UI 요소들을 그 값에 맞게 업데이트
+    var drink: Drink?{
+        didSet{
+            if let drink = drink{
+                self.imageView.image = drink.image
+                self.nameLabel.text = drink.name.0
+                self.priceLabel.text = "\(drink.price)"
+                self.descriptionText.text = drink.description
+            }
+        }
+    }
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -37,28 +36,15 @@ class DetailPageViewController: UIViewController {
           super.viewWillAppear(animated)
 
 
-        //이전 뷰컨트롤러에 있는 정보 가져오기
-        imageView.image = Image
-        nameLabel.text = name
-        priceLabel.text = "\(price)₩"
-        
-        //음료명에 따른 상세설명 표시
-        if let description = descriptions[name]{
-            descriptionText.text = description
-        }else{
-            descriptionText.text = "상세설명 없음"
-        }
-    }
-   
+      }
+
     //segue로 다음 뷰컨트롤러에 정보 전달
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // if segue.identifier == "세그웨이 식별자 이름"
         if segue.identifier == "showNextView"{
-            //if let nextVC = segue.destination as? 다음 뷰 컨트롤러 이름
-//            if let nextVC = segue.destination as? OptionViewController {
-//                nextVC.name = name
-//                nextVC.price = price
-//            }
+            if let nextVC = segue.destination as? OptionViewController{
+                let nextVC = OptionViewController()
+                nextVC.drink = self.drink
+            }
         }
     }
 
