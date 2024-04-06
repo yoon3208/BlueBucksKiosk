6d8f48439a348e25d67ae4293debd0395ea0e94a
hyelim9300 <hye930011@naver.com>
2024-04-07 02:06:06 +0900

    Fix : 불필요한 코드 제거 self.present, resetOtherButtons, 가격: NA

--- BlueBucksKiosk/Controller/OptionViewController.swift
+++ BlueBucksKiosk/Controller/OptionViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class OptionViewController: UIViewController {
     
     // MARK: - Properties
-    
     var price = 0
     var count = 0
     var index = -1
@@ -87,7 +86,6 @@ class OptionViewController: UIViewController {
             manager.addProduct(product: product)
             let mainVC = MainViewController()
             mainVC.modalPresentationStyle = .fullScreen
-//            self.present(mainVC, animated: true)
             self.navigationController?.popToRootViewController(animated: true)
         }
     }
@@ -99,12 +97,6 @@ class OptionViewController: UIViewController {
         button.layer.cornerRadius = 5
         button.layer.borderWidth = button.isSelected ? 2 : 1
         button.layer.borderColor = borderColor
-    }
-    
-    func resetOtherButtons(_ selectedButton: UIButton) {
-        [tallBtn, grandeBtn, ventiBtn].forEach { button in
-            button.layer.borderColor = (button == selectedButton && button.isSelected) ? UIColor.bluebucks.cgColor : UIColor.lightGray.cgColor
-        }
     }
     
     func updateSize() {
@@ -126,7 +118,7 @@ class OptionViewController: UIViewController {
     
     func updateOptionAddPrice(isOptionChosen: Bool) {
         guard let drink = drink else {
-            optionAddPrice.text = "가격: N/A"
+            optionAddPrice.text = ""
             return
         }
         if isOptionChosen {
